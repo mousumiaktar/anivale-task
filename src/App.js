6bf5ce9c3d71a3e6bb30b8dc27963a080b3b684f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-03-29 11:23:37 +0600

    search bar added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 function App() {
   const [imgData, setImgData] = useState([]);
   const [term, setTerm] = useState("");
-  useEffect(()=>{
-      fetch(`https://pixabay.com/api/?key=34814083-66766f21879c55911aa329ca7&q=${term}&image_type=photo&pretty=true`)
-      .then(res=> res.json())
-      .then(data =>console.log(data.hits));
-  },[term]);
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=34814083-66766f21879c55911aa329ca7&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => console.log(data.hits));
+  }, [term]);
   return (
-    <div >
-     
+    <div className='container max-auto'>
+      <div className='flex justify-center'>
+          <input type="search" name="search" id=""
+          placeholder='Search your favourite images....'
+          className='outline-none border-2 gray-600 w-64 py-1 rounded-3xl px-2 mt-2'
+           />
+      </div>
     </div>
   );
 }
