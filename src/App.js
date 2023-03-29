a643f66d6e3feacc82a9176e8b8482e25187c9ad
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-03-29 19:55:29 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import ImgCard from './pages/ImgCard';
+import banner from "./images/island.jpg";
 
 function App() {
   const [imgData, setImgData] = useState([]);
@@ -15,31 +16,39 @@ function App() {
   }, [term]);
 
   return (
-    <div className='container mx-auto'>
-      <div className='flex justify-center'>
-        <input type="search" name="search" id=""
-          onChange={(e) => setTerm(e.target.value)}
-          placeholder='Search your favourite images....'
-          className='outline-none border-2 gray-600 md:w-96 py-1 rounded-3xl px-2 mt-2'
-        />
-      </div>
-      {!isLoading && imgData.length === 0 && (
-        <h1 className="text-center text-4xl font-bold mt-8"><i>No Results Found!</i></h1>
-      )}
-      ;
-      {isLoading ? (
-        <h1 className="text-center text-4xl mt-8 font-bold"><i>Loading.....</i></h1>
-      ) : (
-        <div className='grid md:grid-cols-4 gap-4 mt-8'>
-          {
-            imgData.map((image) => (
-              <ImgCard
-                key={image.id}
-                image={image} />
-            ))
-          }
+    <div className='bg-gray-100'>
+      <div className='container mx-auto'>
+        <div>
+          <img className='h-96 w-full relative' src={banner} alt="" />
+          <div className='flex justify-center'>
+            <h1 className='absolute md:mt-[-250px] hidden md:block text-white text-3xl font-semibold'>Stunning free images & royalty free stock</h1>
+            <input type="search" name="search" id=""
+              onChange={(e) => setTerm(e.target.value)}
+              placeholder='Search your favourite images....'
+              className='outline-none border-2 gray-600 md:w-96  rounded-3xl px-2 py-2 absolute mt-[-200px]'
+            />
+          </div>
         </div>
-      )}
+
+
+        {!isLoading && imgData.length === 0 && (
+          <h1 className="text-center text-4xl font-bold mt-8"><i>No Results Found!</i></h1>
+        )}
+        ;
+        {isLoading ? (
+          <h1 className="text-center text-4xl mt-8 font-bold"><i>Loading.....</i></h1>
+        ) : (
+          <div className='grid md:grid-cols-3 xl:grid-cols-4 gap-4 mt-8'>
+            {
+              imgData.map((image) => (
+                <ImgCard
+                  key={image.id}
+                  image={image} />
+              ))
+            }
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/ImgCard.js
+++ src/pages/ImgCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImgCard = ({ image }) => {
     const { webformatURL } = image;
     return (
-        <div className='bg-gray-300'>
+        <div className='bg-white'>
             <img className='w-full h-52' src={webformatURL} alt="" />
             <p className='my-4 mx-2 text-lg'><strong>Image by:</strong> {image.user}</p>
             <p className='my-4 mx-2'><strong>Tags:</strong> {image.tags}</p>
