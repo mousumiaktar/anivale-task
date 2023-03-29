c84d30373ef1f3f8f3d393dae902c972566fa0ef
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-03-29 11:16:07 +0600

    fatching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [imgData, setImgData] = useState([]);
+  const [term, setTerm] = useState("");
+  useEffect(()=>{
+      fetch(`https://pixabay.com/api/?key=34814083-66766f21879c55911aa329ca7&q=${term}&image_type=photo&pretty=true`)
+      .then(res=> res.json())
+      .then(data =>console.log(data.hits));
+  },[term]);
   return (
     <div >
      
